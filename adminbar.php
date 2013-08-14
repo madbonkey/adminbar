2aa9e74cf80e99aaad87863619ec9ff3040c4be7
Nico Schneider <nico.tcap@gmail.com>
2013-08-14 12:11:39 +0200

    Move css asset to LESS

--- adminbar.php
+++ adminbar.php
@@ -16,8 +16,7 @@ class Plugin_Adminbar extends Plugin {
 	}
 
 	public function debug() {
-
-		return sprintf("<pre>%s</pre>", date("d.m.Y", 1376466965));
+		return;// sprintf("<pre>%s</pre>", date("d.m.Y", 1376466965));
 	}
 
 	public function show() {
@@ -81,22 +80,23 @@ class Plugin_Adminbar extends Plugin {
 
 	public function assets() {
 		if(!$this->_auth()) return;
-		return '<style type="text/css">
-			html { margin-top: 28px !important; }
-			#pcms_admin_bar {position: fixed; height: 28px; background: #464646; background-image: -webkit-gradient(linear,left bottom,left top,color-stop(0,#373737),color-stop(18%,#464646)); background-image: -webkit-linear-gradient(bottom,#373737 0,#464646 5px); background-image: -moz-linear-gradient(bottom,#373737 0,#464646 5px); background-image: -o-linear-gradient(bottom,#373737 0,#464646 5px); background-image: linear-gradient(to top,#373737 0,#464646 5px); color: #dedede; font: normal 13px/28px sans-serif; top: 0; left: 0; width: 100%; min-width: 600px; z-index: 9999; }
-			#menu_left {}
-			.favicon { height: 28px; width: 28px; background: url(\'data:image/x-icon;base64,'.base64_encode(file_get_contents(site_url('favicon.ico'))).'\') no-repeat center center; float: left; margin-right: 5px; }
-			#menu_left, #menu_right { height: 28px; float:left; list-style: none; margin: 0; padding: 0 5px; position: relative; }
-			#menu_right { float: right; }
-			#menu_left  li , #menu_right li { position: relative; float: left; color: #dedede; font: normal 13px/28px sans-serif; border-right: 1px solid #666; }
-			#menu_left li a , #menu_right li a, #menu_right .generic { display: block; padding: 0 15px; border-right: 1px solid #333; color: #dedede; font: normal 13px/28px sans-serif; }
-			#menu_left > li:hover a, #menu_right > li:hover a {background: #222; background-image: -webkit-gradient(linear,left bottom,left top,from(#3a3a3a),to(#222)); background-image: -webkit-linear-gradient(bottom,#3a3a3a,#222); background-image: -moz-linear-gradient(bottom,#3a3a3a,#222); background-image: -o-linear-gradient(bottom,#3a3a3a,#222); background-image: linear-gradient(to top,#3a3a3a,#222); }
-			#menu_left .submenu, #menu_right .submenu { float: none; display: block; position: absolute; top: 28px; left: 0; padding: 5px 0 10px 0; margin: 0; background: #fff;  z-index: 99999; box-shadow: 0 0 5px #555; width: 200px; }
-			#menu_left .submenu li, #menu_right .submenu li { float: none; display: block; height: 28px; border: none;}
-			#menu_left .submenu li a, #menu_right .submenu li a { color: #333; border: none; background: #fff; height: 28px; line-height: 28px;}
-			#menu_left .submenu li a:hover, #menu_right .submenu li a:hover { color: #000; background: #efefef; }
-			#menu_left .submenu, #menu_right .submenu { display: none; }
-			#menu_left li:hover .submenu , #menu_right li:hover .submenu { display: block; }
-		</style>';
+		$css = str_replace(
+			'###FAVICON###',
+			'data:image/x-icon;base64,'.base64_encode( file_get_contents( site_url('favicon.ico') ) ),
+			'html{margin-top:28px !important;}
+#pcms_admin_bar{position:fixed;height:28px;background:#464646;background-image:-webkit-gradient(linear, left bottom, left top, color-stop(0, #373737), color-stop(18%, #464646));background-image:-webkit-linear-gradient(bottom, #373737 0, #464646 5px);background-image:-moz-linear-gradient(bottom, #373737 0, #464646 5px);background-image:-o-linear-gradient(bottom, #373737 0, #464646 5px);background-image:linear-gradient(to top, #373737 0, #464646 5px);color:#dedede;font:normal 13px/28px sans-serif;top:0;left:0;width:100%;min-width:600px;z-index:9999;}#pcms_admin_bar a{font-weight:normal;color:#dedede;font:normal 13px/28px sans-serif;}
+#pcms_admin_bar a:hover{text-decoration:none;text-shadow:none;}
+#pcms_admin_bar .favicon{height:28px;width:28px;background:url(###FAVICON###) no-repeat center center;float:left;margin-right:5px;}
+#pcms_admin_bar #menu_left,#pcms_admin_bar #menu_right{height:28px;float:left;list-style:none;margin:0;padding:0;position:relative;}#pcms_admin_bar #menu_left li,#pcms_admin_bar #menu_right li{position:relative;float:left;color:#dedede;font:normal 13px/28px sans-serif;border-right:1px solid #666;padding:0;margin:0;list-style:none;}#pcms_admin_bar #menu_left li a,#pcms_admin_bar #menu_right li a,#pcms_admin_bar #menu_left li .generic,#pcms_admin_bar #menu_right li .generic{display:block;padding:0 15px;border-right:1px solid #333;color:#dedede;font:normal 13px/28px sans-serif;}
+#pcms_admin_bar #menu_left>li:hover>a,#pcms_admin_bar #menu_right>li:hover>a{background:#222;background-image:-webkit-gradient(linear, left bottom, left top, from(#3a3a3a), to(#222222));background-image:-webkit-linear-gradient(bottom, #3a3a3a, #222222);background-image:-moz-linear-gradient(bottom, #3a3a3a, #222222);background-image:-o-linear-gradient(bottom, #3a3a3a, #222222);background-image:linear-gradient(to top, #3a3a3a, #222222);}
+#pcms_admin_bar #menu_left>li:hover .submenu,#pcms_admin_bar #menu_right>li:hover .submenu{display:block;}
+#pcms_admin_bar #menu_left .submenu,#pcms_admin_bar #menu_right .submenu{float:none;display:block;position:absolute;top:28px;left:0;padding:5px 0 10px 0;margin:0;background:#fff;z-index:99999;box-shadow:0 0 5px #555;width:200px;display:none;}#pcms_admin_bar #menu_left .submenu li,#pcms_admin_bar #menu_right .submenu li{float:none;display:block;height:28px;border:none;}
+#pcms_admin_bar #menu_left .submenu a,#pcms_admin_bar #menu_right .submenu a{color:#333;border:none;background:#fff;height:28px;line-height:28px;}
+#pcms_admin_bar #menu_left .submenu a:hover,#pcms_admin_bar #menu_right .submenu a:hover{color:#000;background:#efefef;}
+#pcms_admin_bar #menu_right{float:right;}
+
+'
+		);
+		return sprintf('<style type="text/css">%s</style>', $css);
 	}
 }
